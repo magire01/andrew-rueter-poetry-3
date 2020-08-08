2cde1f0a9d5eacd20ba15f55f40166d9c5df1562
Mark Gire <magire01@gmail.com>
2020-08-08 02:59:20 -0400

    Fixed poem styling

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ $("#about-nav").on("click", function() {
     aboutPage();
 });
 
+//About Page
 const aboutPage = () => {
     $("#main-page").empty();
     $("#main-page").append("<h3>About Me</h3>");
@@ -30,6 +31,9 @@ const aboutPage = () => {
 
 }
 
+
+
+// poetry page
 const poetryPage = () => {
     $("#main-page").empty();
     $("#main-page").append("<h3>Poetry</h3>");
@@ -63,59 +67,25 @@ const poems = [
     new Poem (1, "Another one", "August 2", "peom text<br>poem Text<br>poemtext<br>poemtext<br>poemtext<br>"),
     new Poem (2, "the first poem", "July 29", "this some serious peom text<br>poem Text<br>poemtext<br>poemtext<br>but this is prob the most important poemtext<br>")
 ];
-// const entries = {
-//     poem1: {
-//         id: 1,
-//         name: "Poem of the Century",
-//         date: "August 4",
-//         text: "peom text\lpoem Text\lpoemtext"
-//     },
-//     poem2: {
-//         id: 2,
-//         name: "Another one",
-//         date: "August 2",
-//         text: "peom text\lpoem Text\lpoemtext\npoemtext\npoemtext\n"
-//     },
-//     poem3: {
-//         id: 3,
-//         name: "the first poem",
-//         date: "July 29",
-//         text: "this some serious peom text\lpoem Text\lpoemtext\npoemtext\nbut this is prob the most important poemtext\n"
-//     }
-// };
 
 const poemEntry = () => {
 
     for(let i = 0; i < poems.length; i++) {
-        $("#main-page").append(`<div class="row">`);
+        $("#main-page").append(`<div class="row post-border">`);
         $("#main-page").append(`<button class="row poem-button" id='poem-btn' data-id=${poems[i].id}>${poems[i].name}</button>`);
-        $("#main-page").append(`<div class="row entryDiv${poems[i].id}" id="entryDiv">`);
+        $("#main-page").append(`<div class="row entryDiv" id="entryDiv${poems[i].id}">`);
         $("#main-page").append(`</div></div>`);
     }
-
-    
-    // $("#main-page").append(`<div class="row poetryDiv">`);
-
-    // $("#main-page").append(`<div class="row entryDiv1">`);
-    // $("#main-page").append(`<button class="row poem-button" id='poem-btn' data-id=${poems[0].id}>${poems[0].name}</button>)`);
-    // poemButton();
-    // $("#main-page").append(`</div>`);
-    // $("#main-page").append(`<button class="row poem-button" id='poem-btn' data-id=${poems[1].id}>${poems[1].name}</button>)`);
-    // $("#main-page").append(`<button class="row poem-button" id='poem-btn' data-id=${poems[2].id}>${poems[2].name}</button>)`);
-    
-    // $("#main-page").append(`</div>`);
 }
 
-// const poemText = () => {
-
-// }
 
 const poemButton = () => {
     $("body").on("click", ".poem-button", function() {
         const id = $(this).data("id");
-        console.log(`Poem text test: ${poems[id].text}`);
-        $(`.entryDiv${id}`).empty();
-        $(`.entryDiv${id}`).append(`<div class="row"><p>${poems[id].text}</p></div>`);
+        //Console Log Test for Poem text
+        // console.log(`Poem text test: ${poems[id].text}`);
+        $(`#entryDiv${id}`).empty();
+        $(`#entryDiv${id}`).append(`<div class="row"><p>${poems[id].text}</p></div>`);
     });
 }
 
