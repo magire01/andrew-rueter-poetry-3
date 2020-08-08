8245e57cd72cb62fddc13036ca18af9c17a14537
Mark Gire <magire01@gmail.com>
2020-08-08 06:27:07 -0400

    Added movie posts to movies page

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ $("#about-nav").on("click", function() {
 //About Page
 const aboutPage = () => {
     $("#main-page").empty();
-    $("#main-page").append(`<div class="col-md-8 blankColWidth"></div><div class="col-md-4"><h3>About Me</h3></div>`);
+    $("#main-page").append(`<div class="col-md-8 blankColWidth"></div><div class="col-md-4"><h3 class="page-title"> About Me </h3></div>`);
     $("#main-page").append("<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime omnis quibusdam praesentium, culpa cupiditate, saepe, facere expedita nostrum neque magnam esse dolore obcaecati voluptatibus ex sint repudiandae cumque accusantium nulla.</p>");
     $("#main-page").append("<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime omnis quibusdam praesentium, culpa cupiditate, saepe, facere expedita nostrum neque magnam esse dolore obcaecati voluptatibus ex sint repudiandae cumque accusantium nulla.</p>");
 
@@ -36,7 +36,7 @@ const aboutPage = () => {
 // poetry page
 const poetryPage = () => {
     $("#main-page").empty();
-    $("#main-page").append(`<div class="col-md-8 blankColWidth"></div><div class="col-md-4"><h3>Poetry</h3></div>`);
+    $("#main-page").append(`<div class="col-md-8 blankColWidth"></div><div class="col-md-4"><h3 class="page-title">Poetry</h3></div>`);
     poemEntry();
     poemButton();
 }
@@ -49,21 +49,7 @@ class Poem {
         this.text = text;
         this.info = info;
     }
-    getId() {
-        return this.id;
-    }
-    getName() {
-        return this.name;
-    }
-    getDate() {
-        return this.date;
-    }
-    getText() {
-        return this.text;
-    }
-    getInfo() {
-        return this.info;
-    }
+    
 }
 
 const poems = [
@@ -73,9 +59,11 @@ const poems = [
     new Poem (3, "Poem of the day", "July 28", "this some serious peom text<br>poem Text<br>poemtext<br>poemtext<br>but this is prob the most important poemtext<br>", "Reddit Link"),
     new Poem (4, "War sucks", "July 19", "this some serious peom text<br>poem Text<br>poemtext<br>poemtext<br>but this is prob the most important poemtext<br>", "Additional Info test"),
     new Poem (5, "Mood, bummed", "July 7", "this some serious peom text<br>poem Text<br>poemtext<br>poemtext<br>but this is prob the most important poemtext<br>", "Additional Info test"),
-    new Poem (6, "Maybe there are too many poems", "July 3", "lets see how wide we can get one linelets see how wide we can get one linelets see how wide we can get one linelets see how wide we can get one linelets see how wide we can get one linelets see how wide we can get one line<br>poem Text<br>poemtext<br>poemtext<br>but this is prob the most important poemtext<br>", "Additional Info test"),
+    new Poem (6, "there are too many poems", "July 3", "lets see how wide we can get one linelets see how wide we can get one linelets see how wide we can get one linelets see how wide we can get one linelets see how wide we can get one linelets see how wide we can get one line<br>poem Text<br>poemtext<br>poemtext<br>but this is prob the most important poemtext<br>", "Additional Info test"),
     new Poem (7, "I'll keep going", "July 1", "lets see how wide we can get one linelets see how wide we can get one linelets see how wide we can get one linelets see how wide we can get one linelets see how wide we can get one linelets see how wide we can get one line<br>poem Text<br>poemtext<br>poemtext<br>but this is prob the most important poemtext<br>", "Additional Info test"),
-    new Poem (8, "Seven poems? these must be very easy to add", "June 20", "lets see how wide we can get one linelets see how wide we can get one linelets see how wide we can get one linelets see how wide we can get one linelets see how wide we can get one linelets see how wide we can get one line<br>poem Text<br>poemtext<br>poemtext<br>but this is prob the most important poemtext<br>", "Additional Info test"),
+    new Poem (8, "the poems", "June 18", "lets see how wide we can get one linelets see how wide we can get one linelets see how wide we can get one linelets see how wide we can get one linelets see how wide we can get one linelets see how wide we can get one line<br>poem Text<br>poemtext<br>poemtext<br>but this is prob the most important poemtext<br>", "Additional Info test"),
+    new Poem (9, "more poems more time", "June 15", "lets see how wide we can get one linelets see how wide we can get one linelets see how wide we can get one linelets see how wide we can get one linelets see how wide we can get one linelets see how wide we can get one line<br>poem Text<br>poemtext<br>poemtext<br>but this is prob the most important poemtext<br>", "Additional Info test"),
+    new Poem (9, "It's over 9 poems", "June 8", "lets see how wide we can get one linelets see how wide we can get one linelets see how wide we can get one linelets see how wide we can get one linelets see how wide we can get one linelets see how wide we can get one line<br>poem Text<br>poemtext<br>poemtext<br>but this is prob the most important poemtext<br>", "Additional Info test"),
 ];
 
 const poemEntry = () => {
@@ -84,7 +72,7 @@ const poemEntry = () => {
         if (i < 2) {
             $("#main-page").append(`<div class="post-border"><button class="row poem-button" id='poem-btn' data-id=${poems[i].id}>${poems[i].name}</button><div class="row entryDiv" id="entryDiv${poems[i].id}"><div class="row"><p>${poems[i].text}</p></div></div><p class="col-md-6 datePosted">Date Posted: ${poems[i].date}</p><p class="col-md-6 datePosted">Additional Info: ${poems[i].info}</p></div>`);
         } else {
-            $("#main-page").append(`<div class="post-border"><button class="row poem-button" id='poem-btn' data-id=${poems[i].id}>${poems[i].name}<span class="readPoem"><p> Read Poem </p></span></button><div class="row entryDiv" id="entryDiv${poems[i].id}"></div><p class="col-md-6 datePosted">Date Posted: ${poems[i].date}</p><p class="col-md-6 datePosted">Additional Info: ${poems[i].info}</p></div>`);
+            $("#main-page").append(`<div class="post-border"><button class="row poem-button" id='poem-btn' data-id=${poems[i].id}>${poems[i].name}<span class="readPoem"><p> Read  </p></span></button><div class="row entryDiv" id="entryDiv${poems[i].id}"></div><p class="col-md-6 datePosted">Date Posted: ${poems[i].date}</p><p class="col-md-6 datePosted">Additional Info: ${poems[i].info}</p></div>`);
         }
         
     }
@@ -104,7 +92,7 @@ const poemButton = () => {
 //story page
 const storyPage = () => {
     $("#main-page").empty();
-    $("#main-page").append(`<div class="col-md-8 blankColWidth"></div><div class="col-md-4"><h3>Short Stories</h3></div>`);
+    $("#main-page").append(`<div class="col-md-8 blankColWidth"></div><div class="col-md-4"><h3 class="page-title">Short Stories</h3></div>`);
     $("#main-page").append("<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime omnis quibusdam praesentium, culpa cupiditate, saepe, facere expedita nostrum neque magnam esse dolore obcaecati voluptatibus ex sint repudiandae cumque accusantium nulla.</p>");
     $("#main-page").append("<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime omnis quibusdam praesentium, culpa cupiditate, saepe, facere expedita nostrum neque magnam esse dolore obcaecati voluptatibus ex sint repudiandae cumque accusantium nulla.</p>");
 
@@ -115,22 +103,59 @@ const storyPage = () => {
 //movies page
 const moviesPage = () => {
     $("#main-page").empty();
-    $("#main-page").append(`<div class="col-md-8 blankColWidth"></div><div class="col-md-4"><h3>Movies</h3></div>`);
-    $("#main-page").append("<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime omnis quibusdam praesentium, culpa cupiditate, saepe, facere expedita nostrum neque magnam esse dolore obcaecati voluptatibus ex sint repudiandae cumque accusantium nulla.</p>");
-    $("#main-page").append("<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime omnis quibusdam praesentium, culpa cupiditate, saepe, facere expedita nostrum neque magnam esse dolore obcaecati voluptatibus ex sint repudiandae cumque accusantium nulla.</p>");
+    $("#main-page").append(`<div class="col-md-8 blankColWidth"></div><div class="col-md-4"><h3 class="page-title">Movies</h3></div>`);
+    movieEntry();
+    movieButton();
 
+}
+
+class Movie {
+    constructor (id, name, date, text, info) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.text = text;
+        this.info = info;
+    }
+    
+}
+
+const movies = [
+    new Movie (0, "Movie Title 1", "August 4", "Movietext Movietext Movietext <br>Movietext <br>Movietext Movietext Movietext Movietext ", "Something something"),
+    new Movie (1, "Movie Title 2", "August 3", "Movietext Movietext Movietext <br>Movietext <br>Movietext Movietext Movietext Movietext ", "Something something"),
+    new Movie (2, "Movie Title 3", "August 2", "Movietext Movietext Movietext <br>Movietext <br>Movietext Movietext Movietext Movietext ", "Something something"),
+    new Movie (3, "Movie Title 4", "August 3", "Movietext Movietext Movietext <br>Movietext <br>Movietext Movietext Movietext Movietext ", "Something something")
+];
+
+const movieEntry = () => {
+    for(let i = 0; i < movies.length; i++) {
+        if (i < 2) {
+            $("#main-page").append(`<div class="post-border"><button class="row movie-button" id='poem-btn' data-id=${movies[i].id}>${movies[i].name}</button><div class="row entryDiv" id="mEntryDiv${movies[i].id}"><div class="row"><p>${movies[i].text}</p></div></div><p class="col-md-6 datePosted">Date Posted: ${movies[i].date}</p><p class="col-md-6 datePosted">Additional Info: ${movies[i].info}</p></div>`);
+        } else {
+            $("#main-page").append(`<div class="post-border"><button class="row movie-button" id='movie-btn' data-id=${movies[i].id}>${movies[i].name}<span class="readPoem"><p> Read </p></span></button><div class="row entryDiv" id="mEntryDiv${movies[i].id}"></div><p class="col-md-6 datePosted">Date Posted: ${movies[i].date}</p><p class="col-md-6 datePosted">Additional Info: ${movies[i].info}</p></div>`);
+        }
+        
+    }
+}
+
+const movieButton = () => {
+    $("body").on("click", ".movie-button", function() {
+        const id = $(this).data("id");
+        //Console Log Test for Poem text
+        console.log(`Movie text test: ${movies[id].text}`);
+        $(`#mEntryDiv${id}`).empty();
+        $(`#mEntryDiv${id}`).append(`<div class="row"><p>${movies[id].text}</p></div>`);
+    });
 }
 
 
 //contact page
 const contactPage = () => {
     $("#main-page").empty();
-    $("#main-page").append(`<div class="col-md-8 blankColWidth"></div><div class="col-md-4"><h3>Contact</h3></div>`);
+    $("#main-page").append(`<div class="col-md-8 blankColWidth"></div><div class="col-md-4"><h3 class="page-title">Contact</h3></div>`);
     $("#main-page").append("<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime omnis quibusdam praesentium, culpa cupiditate, saepe, facere expedita nostrum neque magnam esse dolore obcaecati voluptatibus ex sint repudiandae cumque accusantium nulla.</p>");
     $("#main-page").append("<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime omnis quibusdam praesentium, culpa cupiditate, saepe, facere expedita nostrum neque magnam esse dolore obcaecati voluptatibus ex sint repudiandae cumque accusantium nulla.</p>");
 
 }
 
-
-//
 aboutPage();
